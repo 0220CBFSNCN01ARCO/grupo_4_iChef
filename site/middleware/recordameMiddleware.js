43fb48bef0607b73dd3f41a8010d11233fc6e298
josesalvini <josesalvini@gmail.com>
2020-06-11 22:10:14 -0300

    Add imagenes

--- site/middleware/recordameMiddleware.js
+++ site/middleware/recordameMiddleware.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 function recordameMiddleware (req, res, next){
-    console.log("Estoy en el middleware: usuariologueado " + req.session.usuarioLogueado );
-    console.log("Estoy en el middleware: sesion " + req.session );
+    //console.log("Estoy en el middleware: usuariologueado " + req.session.usuarioLogueado );
+    //console.log("Estoy en el middleware: sesion " + req.session );
     if(req.cookies.recordame != undefined && req.session.usuarioLogueado == undefined){
       let usuariosJSON = fs.readFileSync('./data/users.json',{ encoding:'utf-8'});
       let users;
