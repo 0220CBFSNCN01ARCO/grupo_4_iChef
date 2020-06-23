48905fa8e336ffd920d43d5e205da021c33f044a
josesalvini <josesalvini@gmail.com>
2020-06-23 18:36:14 -0300

    Se modifica header, footer, script base de datos

--- Script_DataBase/data.sql
+++ Script_DataBase/data.sql
@@ -1,4 +1,14 @@
 USE `ichef`;
+
+--
+-- Dumping data for table `users`
+--
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('Admin', 'Admin', 'admin@ichef.com', '$2b$10$7wmTOptmvMBtQ.dWxXenyO8cbrvRMuWUMNk8Me5qQ0pGIbO42ppei', '11111111', 'admin-23-06-2020.jpg', '1');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
 --
 -- Dumping data for table `brand`
 --
